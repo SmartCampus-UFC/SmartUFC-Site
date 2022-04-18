5f2b51497c8701941b32afc6164d6f1abf59fa3d
Michel Bonfim <michelsb@gmail.com>
2022-04-18 22:17:38 +0000

    update import data

--- data/import-data/campus-pici-data.sh
+++ data/import-data/campus-pici-data.sh
@@ -11,22 +11,22 @@ printf "Loading Campus context data "
 #
 # Create Campus Entities
 #
-curl -s -o /dev/null -X POST \
-  'http://orion:1026/v2/op/update' \
-  -H 'Content-Type: application/json' \
-  -g -d '{
-  "actionType": "append",
-  "entities": [
-      {
-	"id":"urn:ngsi-ld:Campus:001",
-        "type":"Campus",
-        "name":{"type":"Text","value":"UFC - Campus do Pici"},
-        "description":{"type":"Text","value":"Universidade Federal do Ceara - Campus do Pici"},
-        "address":{"type":"PostalAddress","value":{"streetAddress":"Av. Mister Hull, s/n","addressRegion":"Fortaleza","addressLocality":"Pici","postalCode":"60455-76"}},
-  	"location":{"type":"geo:json","value":{"type":"Point","coordinates":[-3.74574,-38.57404]}}  		
-      }
-   ]
-}'
+#curl -s -o /dev/null -X POST \
+#  'http://orion:1026/v2/op/update' \
+#  -H 'Content-Type: application/json' \
+#  -g -d '{
+#  "actionType": "append",
+#  "entities": [
+#      {
+#	"id":"urn:ngsi-ld:Campus:001",
+#        "type":"Campus",
+#        "name":{"type":"Text","value":"UFC - Campus do Pici"},
+#        "description":{"type":"Text","value":"Universidade Federal do Ceara - Campus do Pici"},
+#        "address":{"type":"PostalAddress","value":{"streetAddress":"Av. Mister Hull, s/n","addressRegion":"Fortaleza","addressLocality":"Pici","postalCode":"60455-76"}},
+#  	"location":{"type":"geo:json","value":{"type":"Point","coordinates":[-3.74574,-38.57404]}}  		
+#      }
+#   ]
+#}'
 
 #
 # Create a series of Building Entities and place the in each Campus.
@@ -44,8 +44,7 @@ curl -s -o /dev/null -X POST \
           "description":{"type":"Text","value":"Bloco do GREat"},            
           "category":{"type":"Array","value":["classroom", "laboratory", "kitchen", "bathroom", "hall", "projectRoom", "meetingRoom", "telecommunicationsRoom", "facultyRoom"]},
           "location":{"type":"geo:json","value":{"type":"Point","coordinates":[-3.746446746415589, -38.57807075126021]}}, 
-          "totalFloors":{"type":"Integer","value":2},  		
-  	  "refCampus":{"type":"Relationship","value":"urn:ngsi-ld:Campus:001"}
+          "totalFloors":{"type":"Integer","value":2}  		
   	}
    ]
 }'
--- data/import-data/campus-quixada-data.sh
+++ data/import-data/campus-quixada-data.sh
@@ -11,22 +11,22 @@ printf "Loading Campus context data "
 #
 # Create Campus Entities
 #
-curl -s -o /dev/null -X POST \
-  'http://orion:1026/v2/op/update' \
-  -H 'Content-Type: application/json' \
-  -g -d '{
-  "actionType": "append",
-  "entities": [
-      {
-	"id":"urn:ngsi-ld:Campus:002",
-        "type":"Campus",
-        "name":{"type":"Text","value":"UFC - Campus Quixada"},
-        "description":{"type":"Text","value":"Universidade Federal do Ceara - Campus de Quixada"},
-        "address":{"type":"PostalAddress","value":{"streetAddress":"Av. Jose de Freitas Queiroz, 5003","addressRegion":"Quixada","addressLocality":"Cedro","postalCode":"63902-580"}},
-  	"location":{"type":"geo:json","value":{"type":"Point","coordinates":[-4.97847,-39.05645]}}	
-      }
-   ]
-}'
+#curl -s -o /dev/null -X POST \
+#  'http://orion:1026/v2/op/update' \
+#  -H 'Content-Type: application/json' \
+#  -g -d '{
+#  "actionType": "append",
+#  "entities": [
+#      {
+#	"id":"urn:ngsi-ld:Campus:002",
+#        "type":"Campus",
+#        "name":{"type":"Text","value":"UFC - Campus Quixada"},
+#        "description":{"type":"Text","value":"Universidade Federal do Ceara - Campus de Quixada"},
+#        "address":{"type":"PostalAddress","value":{"streetAddress":"Av. Jose de Freitas Queiroz, 5003","addressRegion":"Quixada","addressLocality":"Cedro","postalCode":"63902-580"}},
+#  	"location":{"type":"geo:json","value":{"type":"Point","coordinates":[-4.97847,-39.05645]}}	
+#      }
+#   ]
+#}'
 
 #
 # Create a series of Building Entities and place the in each Campus.
@@ -44,8 +44,7 @@ curl -s -o /dev/null -X POST \
 	  "description":{"type":"Text","value":"Bloco 1 da UFC - Campus Quixada"},
 	  "category":{"type":"Array","value":["classroom", "laboratory", "kitchen", "bathroom", "hall", "projectRoom", "meetingRoom", "telecommunicationsRoom", "facultyRoom"]},
 	  "location":{"type":"geo:json","value":{"type":"Point","coordinates":[-4.979450344624724, -39.05625162484692]}}, 
-	  "totalFloors":{"type":"Integer","value":2},
-	  "refCampus":{"type":"Relationship","value":"urn:ngsi-ld:Campus:002"}
+	  "totalFloors":{"type":"Integer","value":2}
 	}
    ]
 }'
